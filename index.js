38779bcafeaa9ef95c84dc96d4f4e0730caccb0d
Anagha Parkhi <anaghapathak2008@gmail.com>
2018-01-14 07:23:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ function divide(a,b){
  
  function inc(n){ return  } inc()
   function dec(n){ return  } dec()
-    function dec(n){ return  } dec()
+    function makeInt(n){ return  } dec()
       function dec(n){ return  } dec()
